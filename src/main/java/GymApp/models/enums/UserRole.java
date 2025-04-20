6e9df4ac432958edc8a100f9f78be2eac45862a2
MayBasalo <May.Basalo@keyin.com>
2025-04-20 02:43:35 -0230

    Delete src/main directory

--- src/main/java/GymApp/models/enums/UserRole.java
+++
@@ -1,9 +0,0 @@
-package GymApp.models.enums;
-
-public enum UserRole {
-    ADMIN, TRAINER, MEMBER;
-
-    public static UserRole fromString(String value) {
-        return UserRole.valueOf(value.toUpperCase());
-    }
-}
